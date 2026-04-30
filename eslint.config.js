024dc5745a2cf8f3e9d5b54ea1078bda05319895
StanislavDevel <unfeel00@gmail.com>
2026-04-30 18:55:09 +0200

    feat: install and configure Tailwind CSS

--- eslint.config.js
+++ eslint.config.js
@@ -1,23 +1,23 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { defineConfig, globalIgnores } from 'eslint/config'
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import tseslint from "typescript-eslint";
+import { defineConfig, globalIgnores } from "eslint/config";
 // Новые импорты
-import reactX from 'eslint-plugin-react-x'
-import reactDom from 'eslint-plugin-react-dom'
+import reactX from "eslint-plugin-react-x";
+import reactDom from "eslint-plugin-react-dom";
 
 export default defineConfig([
-  globalIgnores(['dist']),
+  globalIgnores(["dist"]),
   {
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{ts,tsx}"],
     extends: [
       js.configs.recommended,
       ...tseslint.configs.recommendedTypeChecked,
       ...tseslint.configs.stylisticTypeChecked,
 
-      reactX.configs['recommended-typescript'],
+      reactX.configs["recommended-typescript"],
       reactDom.configs.recommended,
 
       reactHooks.configs.flat.recommended,
@@ -26,11 +26,10 @@ export default defineConfig([
     languageOptions: {
       globals: globals.browser,
       parserOptions: {
-        project: ['./tsconfig.node.json', './tsconfig.app.json'],
+        project: ["./tsconfig.node.json", "./tsconfig.app.json"],
         tsconfigRootDir: import.meta.dirname,
       },
     },
-    rules: {
-    },
+    rules: {},
   },
-])
+]);
--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite';
-import react, { reactCompilerPreset } from '@vitejs/plugin-react';
-import mdx from '@mdx-js/rollup';
-import babel from '@rolldown/plugin-babel';
+import { defineConfig } from "vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import mdx from "@mdx-js/rollup";
+import babel from "@rolldown/plugin-babel";
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   plugins: [
-    { enforce: 'pre', ...mdx() },
+    { enforce: "pre", ...mdx() },
     react({
       include: /\.(mdx|js|jsx|ts|tsx)$/,
       exclude: [/\/pdf\//, /\.solid\.tsx$/, /\/node_modules\//],
@@ -13,5 +14,6 @@ export default defineConfig({
     babel({
       presets: [reactCompilerPreset()],
     }),
+    tailwindcss()
   ],
 });
